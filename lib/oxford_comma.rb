ee9f1788f96a5e8dfa89f6216bc45e8fc70b902f
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-07 16:35:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,12 @@ def oxford_comma(array)
     with_comma = ""
     array.each_with_index do |word, index|
       if index == array.size - 1
-        with_comma << " and #{word}"
+        with_comma += " and #{word}"
       elsif index == 0
-        with_comma << "#{word},"
+        with_comma += "#{word},"
       else
-        with_comma << " #{word},"
+        with_comma += " #{word},"
       end
     end
+  return with_comma
 end
